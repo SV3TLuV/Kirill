d740013db940cc75f94ec475e00c6de9ebcd5c4c
SV3TLUV <Volk222kloV@gmail.com>
2023-11-27 21:50:49 +0300

    vse ravno

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -7,43 +7,43 @@ namespace Api.Controllers;
 
 public sealed class UserController : BaseController
 {
-    // [HttpPost("{id:int}/change_password")]
-    // public async Task<ActionResult> Post(
-    //     int id,
-    //     [FromBody] ChangePasswordCommand command,
-    //     [FromServices] ApiDbContext context)
-    // {
-    //     var user = await context.Users
-    //         .AsNoTracking()
-    //         .FirstOrDefaultAsync(e => e.Id == id);
-    //
-    //     if (user is null)
-    //     {
-    //         return NotFound();
-    //     }
-    //
-    //     if (command.Password != command.ConfirmPassword)
-    //     {
-    //         return BadRequest();
-    //     }
-    //
-    //     user.Password = command.Password;
-    //
-    //     await context.SaveChangesAsync();
-    //     return Ok();
-    // }
+    [HttpPost("{id:int}/change_password")]
+    public async Task<ActionResult> Post(
+        int id,
+        [FromBody] ChangePasswordCommand command,
+        [FromServices] ApiDbContext context)
+    {
+        var user = await context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == id);
 
-    // [HttpPost("/login")]
-    // public async Task<ActionResult> Post(
-    //     [FromServices] ApiDbContext context)
-    // {
-    //     throw new NotImplementedException();
-    // }
+        if (user is null)
+        {
+            return NotFound();
+        }
 
-    // [HttpPost("/refresh")]
-    // public async Task<ActionResult> Post(
-    //     [FromServices] ApiDbContext context)
-    // {
-    //     throw new NotImplementedException();
-    // }
+        if (command.Password != command.ConfirmPassword)
+        {
+            return BadRequest();
+        }
+
+        user.Password = command.Password;
+
+        await context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpPost("/login")]
+    public async Task<ActionResult> Login(
+        [FromServices] ApiDbContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("/refresh")]
+    public async Task<ActionResult> Refresh(
+        [FromServices] ApiDbContext context)
+    {
+        throw new NotImplementedException();
+    }
 }
