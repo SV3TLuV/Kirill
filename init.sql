684d3a53b6df5754900655ee203443fc28de1ec3
SV3TLUV <Volk222kloV@gmail.com>
2023-11-29 19:56:46 +0300

    fix init.sql

--- init.sql
+++ init.sql
@@ -7,6 +7,9 @@
 
 -- Started on 2023-11-28 19:15:36
 
+CREATE DATABASE labworks;
+\connect labworks;
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
